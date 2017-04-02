cc0bde352c0aa052646add2d22c2d2007144d39b
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-04-02 19:27:34 -0400

    working but song and image still do not change

--- hitchner_apiJukebox.js
+++ hitchner_apiJukebox.js
@@ -1,10 +1,7 @@
-
-
 SC.initialize({
   client_id: 'fd4e76fc67798bfa742089ed619084a6'
 });
-
-
+// My variables
 var playBtn = document.querySelector("#play");
 var pauseBtn = document.querySelector("#pause");
 var backBtn = document.querySelector("#back");
@@ -13,64 +10,64 @@ var info = document.querySelector("#information");
 var userName = document.querySelector("#name");
 var art = document.querySelector("#albumArt");
 var userUrl = document.querySelector("#userUrl");
+var trackName1 = document.querySelector("#songName1");
+var trackName2 = document.querySelector("#songName2");
+var trackName3 = document.querySelector("#songName3");
+var trackName4 = document.querySelector("#songName4");
+var titleGenre = document.querySelector("#title_genre");
+var name;
+var genre;
+var title;
+var description;
+var siteUrl;
 
-
-
-
+// constructor function Song
 function Song (){
+  this.getInfo();
   this.currentSong;
   this.id;
-  this.getSongId();
   this.index = 0;
-  this.playlist = []
+  this.playlist = [];
   this.songUrls = this.playlist;
+  this.trackTitles = this.songNames;
 }
-
-// store urls
-  Song.prototype.getInfo = function (){
+// resolving and displaying playlist info
+Song.prototype.getInfo = function (){
   SC.resolve('https://soundcloud.com/particlehearts/sets/film-rescores').then(function(response) {
     this.index = 0;
-    this.playlist = response.tracks
-    this.songNames = this.playlist[this.index].artwork_url
-    console.log(this.songNames)
-  art.setAttribute("src", "https://i1.sndcdn.com/artworks-000127494156-jnbx7s-large.jpg")
-
-  var name = response.user.username;
-  userName.innerHTML = name + ": Artist";
-  var genre = response.genre
-  var title = response.title;
-  var description = response.description;
-  info.innerHTML = "The genre: " + genre  + "Title: " + title + " Playlist info:" + description
-  var siteUrl = response.permalink_url;
-  userUrl.setAttribute("href",siteUrl)
-
-
- //  var trackName= response.kind;
-
- //  var tracks = response.stream_url;
- // console.log(response.id)
-
-//   titles.innerHTML = "The artist name is " + name + "." + "The artist site is" + userUrl + "The genre is "+ genre  + "the song title is " + title +" the song type is " + trackName + " the track release date is" + trackId + "the description "+ description + id + "hello"+playlistUrls + "silly"+ tracks
-//
-})
-}
-// Getting the Ids of the song from Playlist URL
+    this.playlist = response.tracks;
+    this.playlist[this.index].artwork_url;
+    // Having a hard time finding an eloquent solution to the track names. Still working on it. 
+    trackName1.innerHTML = this.playlist[0].title;
+    trackName2.innerHTML = this.playlist[1].title;
+    trackName3.innerHTML = this.playlist[2].title;
+    trackName4.innerHTML = this.playlist[3].title;
+    name = response.user.username;
+    userName.innerHTML = name + ": Artist";
+    genre = response.genre;
+    title = response.title;
+    titleGenre.innerHTML = title +": "+ genre ;
+    description = response.description;
+    info.innerHTML = " Playlist info:" + description;
+    siteUrl = response.permalink_url;      userUrl.setAttribute("href",siteUrl);
+    art.setAttribute("src", "https://i1.sndcdn.com/artworks-000127494156-jnbx7s-large.jpg");
+  });
+};
+// Getting the Id of the songs from Playlist URL
 Song.prototype.getSongId = function(){
   SC.resolve('https://soundcloud.com/particlehearts/sets/film-rescores').then(function(playlist){
     this.playlist = playlist.tracks
     this.songUrls = this.playlist[this.index].id
     this.getSong(this.songUrls)
   }.bind(this));
-}
+};
 // Passing in the song Id and playing
 Song.prototype.getSong = function(id) {
   SC.stream('/tracks/' + id ).then(function(player) {
     this.currentSong = player;
+    this.currentSong.play();
   }.bind(this));
-}
-Song.prototype.play = function(){
-  this.currentSong.play();
-}
+};
 //Back button
 Song.prototype.back = function() {
   this.currentSong.pause();
@@ -78,14 +75,13 @@ Song.prototype.back = function() {
     this.index--;
     this.songUrls = this.playlist[this.index].id;
     this.getSong(this.songUrls)
-    // titles.innerHTML = songNames[this.index];
   }else if(this.index - 1 < 0) {
     this.index = this.playlist.length - 1
     this.songUrls = this.playlist[this.index].id;
     this.getSong(this.songUrls);
-    // titles.innerHTML = songNames[this.index];
   };
 };
+// Next Button
 Song.prototype.next = function() {
   this.currentSong.pause();
   if (this.index + 1 < this.playlist.length) {
@@ -96,41 +92,31 @@ Song.prototype.next = function() {
     this.index = 0;
     this.songUrls = this.playlist[this.index].id;
     this.getSong(this.songUrls);
-    // titles.innerHTML = songNames[this.index];
   };
 };
-
 // Playing the stream
 Song.prototype.playSong = function() {
-  // this.getSongId();
-  this.play();
-}
+  this.getSongId();
+};
 // Pausing the stream
 Song.prototype.pauseSong = function () {
   this.currentSong.pause();
-
-
-}
-
+};
 playBtn.addEventListener("click", function(e) {
   song.playSong();
-  song.getInfo();
   e.preventDefault();
-})
+});
 pauseBtn.addEventListener("click", function(e) {
   song.pauseSong();
-  console.log("i was clicked");
   e.preventDefault();
-})
+});
 backBtn.addEventListener("click", function(e) {
   song.back();
-  song.playSong();
   e.preventDefault();
-})
+});
 nextBtn.addEventListener("click", function(e) {
   song.next();
-  song.playSong();
   e.preventDefault();
-})
-
+});
+// song object
 var song = new Song;
